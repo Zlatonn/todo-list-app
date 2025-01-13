b16adddc47291e42ad5642a5c2d22164044e2b0e
zlatonn <tonkar2541@hotmail.com>
2025-01-13 16:45:39 +0700

    ✨ Create Todo component
    - create & edit style of Todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="bg-stone-900 min-h-screen w-full m-0 py-20">
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Todo.tsx
@@ -0,0 +1,34 @@
+import todo_icon from "../assets/todo_icon.png";
+
+const Todo = () => {
+  return (
+    <div className="w-11/12 max-w-lg mx-auto min-h-[550px] flex flex-col gap-3 bg-white p-5 rounded-xl ">
+      {/* ---------- Title ---------- */}
+      <div className="flex items-center mt-7 gap-3">
+        <img src={todo_icon} alt="Todo Icon" className="w-10" />
+        <h1 className="text-3xl font-bold">To-Do List App</h1>
+      </div>
+
+      {/* ---------- Task input ---------- */}
+      <div className="flex items-center bg-gray-300 rounded-full mt-7">
+        <input
+          type="text"
+          placeholder="Add you task"
+          className=" bg-transparent border-0 outline-0 flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+        />
+        <button
+          className="border-0 rounded-full bg-orange-600 w-32 h-14
+         text-white text-lg font-medium cursor-pointer"
+        >
+          ADD +
+        </button>
+      </div>
+
+      <hr />
+
+      {/* ---------- Task list ---------- */}
+      <div></div>
+    </div>
+  );
+};
+export default Todo;
