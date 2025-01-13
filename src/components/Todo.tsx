20edf38b7e6b593c01f6241e208fb17b46b65594
zlatonn <tonkar2541@hotmail.com>
2025-01-13 19:10:48 +0700

    ✅ Add feature add task

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,41 @@
+import { useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import TodoItem from "./TodoItem";
 
 const Todo = () => {
+  // Defind task type
+  interface ITask {
+    id: number;
+    task: string;
+    isChecked: boolean;
+  }
+
+  // Create local state
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
+  // Referent task box
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // Function add task
+  const addTask = (): void => {
+    if (inputRef.current) {
+      const inputText = inputRef.current.value.trim();
+
+      if (inputText) {
+        const newTask: ITask = {
+          id: Date.now(),
+          task: inputText,
+          isChecked: false,
+        };
+
+        setTaskList((prev) => [...prev, newTask]);
+
+        // Clear input after adding the task
+        inputRef.current.value = "";
+      }
+    }
+  };
+
   return (
     <div className="w-11/12 max-w-lg mx-auto min-h-[550px] flex flex-col gap-3 bg-white p-5 rounded-xl ">
       {/* ---------- Title ---------- */}
@@ -13,36 +47,25 @@ const Todo = () => {
       {/* ---------- Task input ---------- */}
       <div className="flex items-center bg-gray-300 rounded-full mt-7">
         <input
+          ref={inputRef}
           type="text"
           placeholder="Add you task"
           className=" bg-transparent border-0 outline-0 flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
         />
         <button
+          onClick={addTask}
           className="border-0 rounded-full bg-orange-600 w-32 h-14
          text-white text-lg font-medium cursor-pointer"
         >
           ADD +
         </button>
       </div>
-
       <hr />
-
       {/* ---------- Task list ---------- */}
       <div className="flex flex-col py-3 gap-1">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {taskList.map((task) => (
+          <TodoItem key={task.id} id={task.id} task={task.task} isChecked={task.isChecked} />
+        ))}
       </div>
     </div>
   );
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,13 +1,19 @@
 import notTrick from "../assets/not_tick.png";
-// import trick from "../assets/tick.png";
+import trick from "../assets/tick.png";
 import deleteBtn from "../assets/delete.png";
 
-const TodoItem = () => {
+// Defind props type
+interface TodoItemprops {
+  id: number;
+  task: string;
+  isChecked: boolean;
+}
+const TodoItem = ({ id, task, isChecked }: TodoItemprops) => {
   return (
     <div className="flex justify-between items-center">
       <div className="flex flex-1 items-center gap-3 py-2 px-3 mr-3 rounded-full hover:bg-gray-100 hover:cursor-pointer">
-        <img src={notTrick} alt="notTrick" className="w-6" />
-        <p>Item</p>
+        <img src={isChecked ? trick : notTrick} alt="notTrick" className="w-6" />
+        <p className={`${isChecked ? "line-through" : ""}`}>{task}</p>
       </div>
       <button>
         <img src={deleteBtn} alt="deleteBtn" className="w-4 h-4 hover:scale-150 hover:cursor-pointer" />
